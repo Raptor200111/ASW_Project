5779a0989ab47029e9d7d83bcb788b149006b79c
Huiwen Lin <huiwen.lin@estudiantat.upc.edu>
2024-03-20 16:30:58 +0100

    Added 'Add Link' page visuals and attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_15_175211) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_20_150241) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
   end
 
 end
