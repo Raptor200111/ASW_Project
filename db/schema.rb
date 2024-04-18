e648a0ec3a2bd0923c93a36160912bfd425a1e98
LauraFornaguera <laura.ping.fornaguera@estudiantat.upc.edu>
2024-04-18 21:18:20 +0000

    2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_18_190610) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_18_183459) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -45,6 +45,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_18_190610) do
     t.datetime "updated_at", null: false
     t.integer "votes_up"
     t.integer "votes_down"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "magazines", force: :cascade do |t|
@@ -81,7 +83,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_18_190610) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "vote_articles", force: :cascade do |t|
+ create_table "vote_articles", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "article_id", null: false
     t.string "value"
@@ -89,6 +91,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_18_190610) do
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_vote_articles_on_article_id"
     t.index ["user_id"], name: "index_vote_articles_on_user_id"
+  end
+
+  create_table "vote_comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "comment_id", null: false
+    t.string "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["comment_id"], name: "index_vote_comments_on_comment_id"
+    t.index ["user_id"], name: "index_vote_comments_on_user_id"
   end
 
   add_foreign_key "articles", "magazines"
