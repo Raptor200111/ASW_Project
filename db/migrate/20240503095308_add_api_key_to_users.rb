a768f79a41acd2a0ea267b26ec47ded68516384a
LauraFornaguera <laura.ping.fornaguera@estudiantat.upc.edu>
2024-05-04 16:01:05 +0000

    magazines

--- db/migrate/20240503095308_add_api_key_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddApiKeyToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :api_key, :string, unique: true
-    User.find_each do |u|
-      u.update_columns(api_key: SecureRandom.base58(24))
-    end
-  end
-end
