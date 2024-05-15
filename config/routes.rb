4b920075d131d209f984804f07f959f31c761913
Liliu000 <maria.liliu.martinez@estudiantat.upc.edu>
2024-05-15 14:39:10 +0000

    voteArticle controller + index voteArticle

--- config/routes.rb
+++ config/routes.rb
@@ -45,6 +45,7 @@ Rails.application.routes.draw do
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :vote_articles
 
   get 'commentOrder', to: 'articles#commentOrder'
 
