2666e060fb5011de89b0abd4d6308f8256af60a7
LauraFornaguera <160223757+LauraFornaguera@users.noreply.github.com>
2024-05-17 01:11:23 +0200

    add skip

--- app/controllers/magazines_controller.rb
+++ app/controllers/magazines_controller.rb
@@ -1,4 +1,5 @@
 class MagazinesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_user, only: [:create, :subscribe, :unsubscribe ]
   before_action :set_magazine, only: %i[ show edit update subscribe unsubscribe]
 
