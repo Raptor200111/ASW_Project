346565f1db5f5b6ff5c440248a31a122203f7af0
alvardalda <alvar.dalda@estudiantat.upc.edu>
2024-05-17 00:38:14 +0000

    Revert "fix"
    This reverts commit 0cdf456f79a0fbf5ac90cc8dcee05c937ccb1f61.

--- app/controllers/magazines_controller.rb
+++ app/controllers/magazines_controller.rb
@@ -72,12 +72,13 @@ class MagazinesController < ApplicationController
     begin
       if isSubs
         respond_to do |format|
-          format.json { render(json: {"error": "Already subscribed"}, status: 201)}
+          format.json { render(json: {"error": "Already subscribed"}, status: 204)}
         end
       else
         @user.subs << @magazine
         respond_to do |format|
-          format.json { render(json: {"Subscribed successfully"}, status: 201)}
+          format.html {redirect_to magazines_path, notice: 'Subscribed successfully!'}
+          format.json {head :no_content }
         end
       end
     end
@@ -85,13 +86,15 @@ class MagazinesController < ApplicationController
 
   # DELETE /magazines/1/unsubscribe
   def unsubscribe
+    @user = User.find(api_key: request.headers['Authorization'])
     isSubs = @user.subscriptions.find_by(magazine: @magazine)
     begin
       if isSubs
         isSubs.destroy
         @user.subs.delete(@magazine)
         respond_to do |format|
-          format.json { render(json: {"Unbscribed successfully"}, status: 204)}
+          format.html {redirect_to magazines_path, notice: 'Unsubscibed successfully!'}
+          format.json {head :no_content }
         end
       else
         respond_to do |format|
